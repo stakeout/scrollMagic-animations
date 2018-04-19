f12da335849afad0108196d5f3f93da8698ed480
andpost2000html <andpost2000@mail.ru>
2018-04-19 12:26:26 +0300

    new

--- markup/components/example/example.js
+++ markup/components/example/example.js
@@ -2,11 +2,11 @@ var controller = new ScrollMagic.Controller();
 
 
   // build tween
-  var tween = TweenMax.to(".promo", 1, {css: {scale: 0}, ease: Linear.easeNone});
+  var tween = TweenMax.to(".scale", 1, {css: {scale: 0}, ease: Linear.easeNone});
 
 var ourScene = new ScrollMagic.Scene({
-    triggerElement: '.elem',
-    triggerHook: 0.9,
+    triggerElement: '.promo-wrap',
+    triggerHook: 0,
     duration: '100%'
 })
 .setClassToggle('#promo', 'fade-in')
